fd31bb64e148a5fa5836291677ceccc2c24a1a0b
Ajordon11 <andrej.fula@gmail.com>
2022-06-03 10:59:16 +0200

    feat: add basic CRUD operations for recipes and components, prepare dummy files as local DB

--- server/api.js
+++ server/api.js
@@ -1,11 +1,142 @@
+'use strict'
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const bigArray = prepareData();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 router.get('/filter', (req, res) => {
     console.log('request: ', req.query.input);
     res.json({response: 'yes'});
+})
+
+router.get('/components', (req, res) => {
+    fs.readFile('components.json',(err, data) => {
+        if (err) throw err;
+        const components = JSON.parse(data);
+        console.log(components);
+        res.json(components);
+    });
+})
+
+router.put('/components/:id', (req, res) => {
+    fs.readFile('components.json',(err, data) => {
+        if (err) throw err;
+        const components = JSON.parse(data);
+        const index = components.findIndex(c => c.id === req.params.id);
+        const changedComponent = {
+            ...components[index],
+            ...req.body
+        };
+        components[index] = changedComponent;
+        fs.writeFile('components.json', JSON.stringify(components), (err) => {
+            if (err) throw err;
+            console.log('Component ' + changedComponent.id + ' changed: ', changedComponent);
+            res.json(changedComponent);
+        });
+    });
+})
+
+router.delete('/components/:id', (req, res) => {
+    fs.readFile('components.json',(err, data) => {
+        if (err) throw err;
+        const components = JSON.parse(data);
+        const index = components.findIndex(c => c.id === req.params.id);
+        components.splice(index, 1);
+        fs.writeFile('components.json', JSON.stringify(components), (err) => {
+            if (err) throw err;
+            console.log('Component ' + req.params.id + ' removed');
+            res.status(204);
+            res.send();
+        });
+    });
+})
+
+router.post('/components', (req, res) => {
+    fs.readFile('components.json', (err, data) => {
+        if (err) throw err;
+        const components = JSON.parse(data);
+        const id = Math.max(...components.map(o => parseInt(o.id))) + 1;
+        const no = Math.max(...components.map(o => o.no)) + 1;
+        const newRecipe = {
+            no,
+            id: String(id).padStart(10, '0'),
+            ...req.body
+        }
+        components.push(newRecipe);
+        fs.writeFile('components.json', JSON.stringify(components), (err) => {
+            if (err) throw err;
+            console.log('New component added: ', newRecipe);
+            res.json(newRecipe);
+        });
+    })
+})
+
+router.get('/recipes', (req, res) => {
+    fs.readFile('recipes.json',(err, data) => {
+        if (err) throw err;
+        let recipes = JSON.parse(data);
+        console.log(recipes);
+        res.json(recipes);
+    });
+})
+
+router.put('/recipes/:id', (req, res) => {
+    fs.readFile('recipes.json',(err, data) => {
+        if (err) throw err;
+        const recipes = JSON.parse(data);
+        const index = recipes.findIndex(c => c.id === req.params.id);
+        const changedRecipe = {
+            id: recipes[index].id,
+            no: recipes[index].no,
+            ...req.body
+        };
+        recipes[index] = changedRecipe;
+        fs.writeFile('recipes.json', JSON.stringify(recipes), (err) => {
+            if (err) throw err;
+            console.log('Recipe ' + changedRecipe.id + ' changed: ', changedRecipe);
+            res.json(changedRecipe);
+        });
+    });
+})
+
+router.delete('/recipes/:id', (req, res) => {
+    fs.readFile('recipes.json',(err, data) => {
+        if (err) throw err;
+        const recipes = JSON.parse(data);
+        const index = recipes.findIndex(c => c.id === req.params.id);
+        recipes.splice(index, 1);
+        fs.writeFile('recipes.json', JSON.stringify(recipes), (err) => {
+            if (err) throw err;
+            console.log('Recipe ' + req.params.id + ' removed');
+            res.status(204);
+            res.send();
+        });
+    });
+})
+
+router.post('/recipes', (req, res) => {
+    fs.readFile('recipes.json', (err, data) => {
+        if (err) throw err;
+        const recipes = JSON.parse(data);
+        const id = Math.max(...recipes.map(o => parseInt(o.id))) + 1;
+        const no = Math.max(...recipes.map(o => o.no)) + 1;
+        const newRecipe = {
+            no,
+            id: String(id).padStart(10, '0'),
+            ...req.body
+        }
+        recipes.push(newRecipe);
+        fs.writeFile('recipes.json', JSON.stringify(recipes), (err) => {
+            if (err) throw err;
+            console.log('New recipe added: ', newRecipe);
+            res.json(newRecipe);
+        });
+    })
 })
 
 router.get('/data', (req, res) => {
