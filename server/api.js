5cced9f69adcfd39100b14240a383d81b186784b
Ajordon11 <andrej.fula@gmail.com>
2022-06-13 23:39:42 +0200

    refactor: change recipes and components to manyToMany relation with extra table

--- server/api.js
+++ server/api.js
@@ -4,18 +4,11 @@ const router = express.Router();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-const bigArray = prepareData();
-
-router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
-router.get('/filter', (req, res) => {
-    console.log('request: ', req.query.input);
-    res.json({response: 'yes'});
-})
-
 router.get('/components', (req, res) => {
-    fs.readFile('components.json',(err, data) => {
+    fs.readFile('components.json', (err, data) => {
         if (err) throw err;
         const components = JSON.parse(data);
         console.log(components);
@@ -23,14 +16,15 @@ router.get('/components', (req, res) => {
     });
 })
 
-router.put('/components/:ID', (req, res) => {
-    fs.readFile('components.json',(err, data) => {
+router.put('/components/:id', (req, res) => {
+    fs.readFile('components.json', (err, data) => {
         if (err) throw err;
         const components = JSON.parse(data);
         const index = components.findIndex(c => c.id === req.params.id);
         const changedComponent = {
             ...components[index],
-            ...req.body
+            ...req.body,
+            lastUpdate: new Date()
         };
         components[index] = changedComponent;
         fs.writeFile('components.json', JSON.stringify(components), (err) => {
@@ -41,8 +35,8 @@ router.put('/components/:ID', (req, res) => {
     });
 })
 
-router.delete('/components/:ID', (req, res) => {
-    fs.readFile('components.json',(err, data) => {
+router.delete('/components/:id', (req, res) => {
+    fs.readFile('components.json', (err, data) => {
         if (err) throw err;
         const components = JSON.parse(data);
         const index = components.findIndex(c => c.id === req.params.id);
@@ -60,11 +54,10 @@ router.post('/components', (req, res) => {
     fs.readFile('components.json', (err, data) => {
         if (err) throw err;
         const components = JSON.parse(data);
-        const id = Math.max(...components.map(o => parseInt(o.id))) + 1;
         const no = Math.max(...components.map(o => o.no)) + 1;
         const newRecipe = {
             no,
-            id: String(id).padStart(10, '0'),
+            lastUpdate: new Date(),
             ...req.body
         }
         components.push(newRecipe);
@@ -76,36 +69,61 @@ router.post('/components', (req, res) => {
     })
 })
 
+// toto spravi join medzi tabulkami, je to klasicky manyToMany mapping
 router.get('/recipes', (req, res) => {
-    fs.readFile('recipes.json',(err, data) => {
+    fs.readFile('recipes.json', (err, recipesData) => {
         if (err) throw err;
-        let recipes = JSON.parse(data);
-        console.log(recipes);
-        res.json(recipes);
+        let recipes = JSON.parse(recipesData);
+        fs.readFile('components.json', (err, componentsData) => {
+            if (err) throw err;
+            const components = JSON.parse(componentsData);
+            fs.readFile('recipe-components.json', (err, mappingData) => {
+                if (err) throw err;
+                const mapping = JSON.parse(mappingData);
+                recipes = recipes.map(r => {
+                    const selectedComponents = mapping.filter(m => m.recipeId === r.id);
+                    return {
+                        ...r,
+                        components: selectedComponents.map(c => {
+                            const component = components.find(s => s.id === c.componentId);
+                            return {
+                                ...component,
+                                componentSP: c.componentSP
+                            }
+                        })
+                    }
+                })
+                console.log(recipes);
+                res.json(recipes);
+            })
+        })
     });
 })
 
-router.put('/recipes/:ID', (req, res) => {
-    fs.readFile('recipes.json',(err, data) => {
+
+router.put('/recipes/:id', (req, res) => {
+    fs.readFile('recipes.json', (err, data) => {
         if (err) throw err;
         const recipes = JSON.parse(data);
         const index = recipes.findIndex(c => c.id === req.params.id);
         const changedRecipe = {
-            id: recipes[index].id,
             no: recipes[index].no,
-            ...req.body
+            id: req.body.id,
+            name: req.body.name,
+            lastUpdate: new Date(),
         };
         recipes[index] = changedRecipe;
         fs.writeFile('recipes.json', JSON.stringify(recipes), (err) => {
             if (err) throw err;
             console.log('Recipe ' + changedRecipe.id + ' changed: ', changedRecipe);
+            changeComponents(req.params.id, req.body.components);
             res.json(changedRecipe);
         });
     });
 })
 
-router.delete('/recipes/:ID', (req, res) => {
-    fs.readFile('recipes.json',(err, data) => {
+router.delete('/recipes/:id', (req, res) => {
+    fs.readFile('recipes.json', (err, data) => {
         if (err) throw err;
         const recipes = JSON.parse(data);
         const index = recipes.findIndex(c => c.id === req.params.id);
@@ -113,83 +131,71 @@ router.delete('/recipes/:ID', (req, res) => {
         fs.writeFile('recipes.json', JSON.stringify(recipes), (err) => {
             if (err) throw err;
             console.log('Recipe ' + req.params.id + ' removed');
+            removeComponents(req.params.id);
             res.status(204);
             res.send();
         });
     });
 })
 
+
 router.post('/recipes', (req, res) => {
     fs.readFile('recipes.json', (err, data) => {
         if (err) throw err;
         const recipes = JSON.parse(data);
-        const id = Math.max(...recipes.map(o => parseInt(o.id))) + 1;
         const no = Math.max(...recipes.map(o => o.no)) + 1;
         const newRecipe = {
             no,
-            id: String(id).padStart(10, '0'),
-            ...req.body
+            id: req.body.id,
+            name: req.body.name,
+            lastUpdate: new Date(),
         }
         recipes.push(newRecipe);
         fs.writeFile('recipes.json', JSON.stringify(recipes), (err) => {
             if (err) throw err;
             console.log('New recipe added: ', newRecipe);
+            addComponentsToRecipe(newRecipe.id, req.body.components);
             res.json(newRecipe);
         });
     })
 })
 
-router.get('/data', (req, res) => {
-    // console.log('query: ', req.query)
-    const filterId = req.query.search?.value | null; // filter value (from search field)
-    // console.log('filter ', filterId);
-    let data;
-    if (filterId) {
-        data = bigArray.filter(e => e.value.indexOf(filterId) > -1);
-    } else {
-        data = bigArray;
-    }
-    // SELECT * FROM ... LIMIT = req.query.length OFFSET = req.query.start
-    if (req.query.start != null && req.query.length != null) {
-        data = JSON.stringify(data.slice(req.query.start, +req.query.start + +req.query.length));
-    } else {
-        data = JSON.stringify(data);
-    }
-    const recordsTotal = bigArray.length; // from DB
-    const recordsFiltered = data.length;
-    res.json({
-        'draw': parseInt(req.query.draw),
-        'recordsFiltered': recordsFiltered,
-        'recordsTotal': recordsTotal,
-        'data': JSON.parse(data) // needs to be JSON array
+function removeComponents(id) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('recipe-components.json', (err, data) => {
+            if (err) throw err;
+            let mapping = JSON.parse(data);
+            mapping = mapping.filter(c => c.recipeId !== id);
+            fs.writeFile('recipe-components.json', JSON.stringify(mapping), (err) => {
+                if (err) throw err;
+                console.log('Components mappings for recipe ' + id + ' removed');
+                resolve();
+            });
+        });
+    });
+}
+
+function addComponentsToRecipe(id, components) {
+    fs.readFile('recipe-components.json', (err, data) => {
+        if (err) throw err;
+        let mapping = JSON.parse(data);
+        components.forEach(c => {
+            mapping.push({
+                recipeId: id,
+                componentId: c.id,
+                componentSP: c.componentSP
+            });
+        });
+        fs.writeFile('recipe-components.json', JSON.stringify(mapping), (err) => {
+            if (err) throw err;
+            console.log('Components mappings for recipe ' + id + ' added');
+        })
     })
-})
+}
 
-router.delete('/data/:ID', ((req, res) => {
-    // SQL Delete, remove stuff below
-    const index = bigArray.findIndex(e => e.id === +req.params.id);
-    console.log('delete ID: ' + req.params.id + ' at index: ' + index);
-    bigArray.splice(index, 1);
-    console.log(bigArray.slice(0, 10)); // just to check first 10
-    res.send()
-}))
-
-function prepareData() {
-    const arr = [];
-    let obj;
-    for (let i = 0; i < 10000; i++) {
-        obj = {
-            no: i,
-            id: String(i).padStart(5, '0'),
-            name: 'name' + i
-        }
-        for (let j = 0; j < Math.floor(Math.random() * (30 - 1 + 1)) + 1; j++) {
-            obj['componentName' + j] = 'component' + j;
-            obj['componentSP' + j] = j;
-        }
-        arr.push(obj)
-    }
-    return arr;
+async function changeComponents(id, components) {
+    await removeComponents(id);
+    addComponentsToRecipe(id, components);
 }
 
 module.exports = router;
